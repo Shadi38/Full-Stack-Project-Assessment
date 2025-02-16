ae10f38c2abde53e2fa1ea0d9e2a35882ed727ef
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-02-16 11:02:00 +0000

    added the inbound role to recommendationEc2

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -116,6 +116,12 @@ resource "aws_security_group" "recommendationEc2" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow port 3000 from anywhere (modify as needed)
   }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
