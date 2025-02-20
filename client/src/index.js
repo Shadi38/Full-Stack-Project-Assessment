1549dd2a1f87a8089ad5e447b9599842959c2fdf
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-02-20 11:54:19 +0000

    changed the index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,26 @@
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-
-// ReactDOM.render(
-//   <BrowserRouter> {/* Wrapping App component with BrowserRouter */}
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom"; 
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter> {/* Wrapping App component with BrowserRouter */}
     <App />
-  </HashRouter>,
-  document.getElementById("root")
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { HashRouter } from "react-router-dom"; 
+// import App from "./App";
+
+// ReactDOM.render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>,
+//   document.getElementById("root")
+// );
