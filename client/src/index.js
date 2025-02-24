51ea825e5afa9a4324438714a6104e33af00711e
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-02-24 22:32:49 +0000

    deleted history.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,30 +11,18 @@
 //   </BrowserRouter>,
 //   document.getElementById('root')
 // );
-// import React from 'react';
-// import ReactDOM from 'react-dom/client'; 
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-
-// // Use createRoot instead of render
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter basename="/">
-//     <App />
-//   </BrowserRouter>
-// );
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-import history from './history';
-import App from './App';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
+// Use createRoot instead of render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HistoryRouter history={history}>
+  <BrowserRouter basename="/">
     <App />
-  </HistoryRouter>
+  </BrowserRouter>
 );
+
 
