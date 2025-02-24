e45cc67539659d5764a7c830f859e3c9ef27414a
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2025-02-24 10:40:08 +0000

    changed the package.json in client folder

--- client/src/index.js
+++ client/src/index.js
@@ -13,14 +13,4 @@ ReactDOM.render(
 );
 
 
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { HashRouter } from "react-router-dom"; 
-// import App from "./App";
-
-// ReactDOM.render(
-//   <HashRouter>
-//     <App />
-//   </HashRouter>,
-//   document.getElementById("root")
-// );
+/
